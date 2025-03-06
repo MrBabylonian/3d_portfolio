2e08ce628932516173ce6d5c7571c391ebc816d7
MrBabylonian <bedirhangilgiler@gmail.com>
2025-03-06 01:20:36 +0100

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-// filepath: c:\Users\bedir\OneDrive\Desktop\3d_portfolio\main.ts
-import { serve } from "https://deno.land/std@0.217.0/http/server.ts";
+import { serve } from "https://deno.land/x/http@0.217.0/server.ts";
 import { serveDir } from "https://deno.land/std@0.217.0/http/file_server.ts";
 
 const handler = async (request: Request): Promise<Response> => {
-    const url = new URL(request.url);
     return serveDir(request, {
         fsRoot: "./dist", // Change this to the directory where Vite outputs the build files
         urlRoot: "",
