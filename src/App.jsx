06fbeb48dd5915b22a2fb5f56b08f98dbae43f92
MrBabylonian <bedirhangilgiler@gmail.com>
2025-03-11 18:25:46 +0100

    Routing configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar.jsx";
 import { About, Contact, Home } from "./constants/pages_index.js";
 
 function App() {
     return (
         <main className="min-h-[600px] min-w-[400px]">
-            <BrowserRouter>
+            <HashRouter>
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="about" element={<About />} />
-                    <Route path="Contact" element={<Contact />} />
+                    <Route path="contact" element={<Contact />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </main>
     );
 }
