c0154b6a302d38eb6ebfcb9473f85001303d67c2
MrBabylonian <bedirhangilgiler@gmail.com>
2025-07-09 14:57:46 +0200

    CV update

--- src/constants/experiences_index.js
+++ src/constants/experiences_index.js
@@ -1,9 +1,4 @@
 import scaleAiIcon from "../assets/icons/experiences/scale.jpg";
-import alfredoIcon from "../assets/icons/experiences/alfredo.png";
-import melogranoIcon from "../assets/icons/experiences/melograno.png";
-import sheratonIcon from "../assets/icons/experiences/sheraton.png";
-import eskidjiIcon from "../assets/icons/experiences/eskidji.jpg";
-import atasunIcon from "../assets/icons/experiences/atasun.png";
 import abivetIcon from "../assets/icons/experiences/Abivet.svg";
 
 export const experiences = [
@@ -15,10 +10,12 @@ export const experiences = [
         bottomColor: "#1765d1",
         date: "April 2025 - Present",
         details: [
-            "Designing and developing end-to-end computer vision solutions for medical imaging using Python, FastAI, and PyTorch.",
-            "Building and deploying machine learning models for condition detection via FastAPI.",
-            "Implementing full-stack systems with Java, Spring, and Python for scalable model serving.",
-            "Collaborating across teams and validating on diverse datasets while integrating the latest AI advancements.",
+            "Engineered and developed end-to-end computer vision applications focused on medical imaging and diagnostics.",
+            "Built and optimized deep learning models for the automated detection and classification of medical conditions using Python, PyTorch, and FastAI.",
+            "Developed and deployed full-stack applications to serve machine learning models, utilizing Python (FastAPI), Java (Spring), TypeScript(React), and Dart(Flutter).",
+            "Collaborated with cross-functional teams of medical experts and product managers to deliver tailored software solutions.",
+            "Ensured model robustness and accuracy through rigorous validation on diverse medical imaging datasets.",
+            "Researched and integrated state-of-the-art AI and computer vision techniques to continuously improve solution efficacy.",
         ],
     },
     {
@@ -27,82 +24,12 @@ export const experiences = [
         icon: scaleAiIcon,
         iconBg: "#FFFFFF",
         bottomColor: "#1765d1",
-        date: "March 2024 - Aprile 2025",
+        date: "March 2023 - Aprile 2025",
         details: [
-            "Training AI models to improve coding capabilities and tackle diverse tasks.",
-            "Building full-stack apps with Java, Python, React, and Django.",
-            "Managing databases, version control, and backend frameworks like Spring.",
-            "Leveraging expertise in debugging, OOP, and modern web development with Tailwind CSS and SQL.",
-        ],
-    },
-    {
-        title: "Chef de Rang",
-        company_name: "Alfredo Alla Scrofa",
-        icon: alfredoIcon,
-        iconBg: "#FFFFFF",
-        bottomColor: "#315c3b",
-        date: "2021 May - 2024 October",
-        details: [
-            "Led front-of-house operations, ensuring seamless service and team coordination.",
-            "Drove sales through expert upselling, premium wine pairings, and personalized guest experiences.",
-            "Delivered high-end service to high-profile guests, including preparing signature dishes.",
-            "Managed reservations, optimized dining room flow, and maintained top-tier quality standards.",
-        ],
-    },
-    {
-        title: "Pizza Chef",
-        company_name: "Il Melograno",
-        icon: melogranoIcon,
-        iconBg: "#FFFFFF",
-        bottomColor: "#cf132f",
-        date: "2020 January - 2021 April",
-        details: [
-            "Mastered the art of crafting authentic Roman-style pizzas with top-quality ingredients.",
-            "Expertly prepared and baked both sliced and round pizzas, earning praise from critics and loyal customers.",
-            "Preserved traditional techniques while innovating to satisfy modern tastes.",
-            "Maintained high efficiency and consistency, ensuring top-tier quality even during peak hours.",
-        ],
-    },
-    {
-        title: "International Sales Specialist",
-        company_name: "Sheraton Hotels & Resorts",
-        icon: sheratonIcon,
-        iconBg: "#FFFFFF",
-        bottomColor: "#847b87",
-        date: "April 2017 - October 2019",
-        details: [
-            "Sold luxury residential properties to high-net-worth international clients, exceeding sales targets.",
-            "Conducted market analysis to identify opportunities and provide strategic property recommendations.",
-            "Managed title transfer processes, ensuring smooth and transparent transactions.",
-            "Built strong client relationships through exceptional service and targeted marketing strategies.",
-        ],
-    },
-    {
-        title: "Real Estate Agent",
-        company_name: "Eskidji Real Estate",
-        icon: eskidjiIcon,
-        iconBg: "#FFFFFF",
-        bottomColor: "#dbb248",
-        date: "2014 July - March 2017",
-        details: [
-            "Curated a portfolio of high-end residential and commercial properties to meet the expectations of high-net-worth clients.",
-            "Conducted market analysis to assess property value and ensure exclusivity.",
-            "Consistently exceeded sales targets through strong negotiation and client relationship management.",
-            "Managed title transfers efficiently, ensuring seamless real estate transactions.",
-        ],
-    },
-    {
-        title: "Sales Specialist",
-        company_name: "Atasun Optik",
-        icon: atasunIcon,
-        iconBg: "#FFFFFF",
-        bottomColor: "#eb7713",
-        date: "March 2012 - June 2014 ",
-        details: [
-            "Consistently exceeded sales targets by leveraging expert product knowledge, strategic upselling, and cross-selling techniques to drive revenue and maximize customer value.",
-            "Provided personalized service, identified customer needs, and developed tailored solutions to enhance satisfaction, retention, and long-term loyalty.",
-            "Became a trusted advisor by acquiring in-depth knowledge of optical products, offering expert recommendations, and ensuring customers made informed purchasing decisions.",
-            "Managed follow-up processes to address customer concerns, enhance brand reputation, and worked closely with team members to improve overall sales performance.",
+            "Trained and fine-tuned AI models to augment coding capabilities and automate complex software development tasks.",
+            "Engineered and developed scalable full-stack applications and microservices using Java (Spring), Python (Django), and TypeScript(React).",
+            "Developed modern, responsive user interfaces by integrating front-end frameworks like React with Tailwind CSS.",
+            "Managed robust backend systems, applying expertise in Object-Oriented Programming (OOP), database management (SQL), and advanced debugging.",
         ],
     },
 ];
